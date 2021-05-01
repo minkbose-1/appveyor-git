812ac8737b4fa2362b8a082fd928bcbb4fcb96fc
EvaMink <76566995+EvaMink@users.noreply.github.com>
2021-05-01 03:33:09 +0000

    Update cache_time.ps1

--- build/cache_time.ps1
+++ build/cache_time.ps1
@@ -4,7 +4,7 @@ $LAST_HASH = git log ${env:APPVEYOR_REPO_BRANCH} -1 --format="%H"
 
 # Reset shallow state
 git reset ${env:APPVEYOR_REPO_BRANCH} --hard
-git fetch origin ${env:APPVEYOR_REPO_BRANCH} --depth=1 --no-tags --quiet
+git fetch origin ${env:APPVEYOR_REPO_BRANCH} --depth=10 --no-tags --quiet
 
 
 
@@ -12,21 +12,24 @@ git fetch origin ${env:APPVEYOR_REPO_BRANCH} --depth=1 --no-tags --quiet
 
 
 
+# Find merge-base commit manually
 do {
-  git fetch origin ${env:APPVEYOR_REPO_BRANCH} --deepen=100 --no-tags --quiet
+  $HASH_LIST = git log $LAST_HASH -25 --format="%H"
 
 
-  $HASH_LIST = git log $LAST_HASH -100 --format="%H"
-
-
-  ForEach( $LAST_HASH in $HASH_LIST ) {
+  ForEach ($LAST_HASH in $HASH_LIST) {
     $LastTime = git log $LAST_HASH -1 --format="%ci"
 
-    if( $LastTime -Le $REPO_MASTER_TIME ) {
+    if ($LastTime -Le $REPO_MASTER_TIME) {
       break
     }
   }
-} while( $LastTime -Gt $REPO_MASTER_TIME )
+
+
+  if ($LastTime -Gt $REPO_MASTER_TIME) {
+    git fetch origin ${env:APPVEYOR_REPO_BRANCH} --deepen=25 --no-tags --quiet
+  }
+} while ($LastTime -Gt $REPO_MASTER_TIME)
 
 
 
