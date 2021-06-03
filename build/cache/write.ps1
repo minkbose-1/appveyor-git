ab26fa64a80711068002619c0bed9994f8b73700
EvaMink <76566995+EvaMink@users.noreply.github.com>
2021-06-03 01:20:38 +0000

    Update write.ps1

--- build/cache/write.ps1
+++ build/cache/write.ps1
@@ -6,6 +6,9 @@ run_script "build/cache/cache_time.ps1"
 
 
 
+#############################################
+
+
 
 echo "`n"
 echo "Saving build progress`n"
@@ -21,20 +24,6 @@ echo "Saving build progress`n"
 
 7z a "cache.zip" "-tzip" "-mx=1" "-y" "cache.hash" > $Null
 
-
-
-
-# $CACHE_LIST = git ls-files --others --exclude-standard
-# $CACHE_LIST += git ls-files --modified --exclude-standard
-
-
-# echo "`n"
-# echo $CACHE_LIST
-
-
-if ($CACHE_LIST -Eq $Null) {
-  # return
-}
 
 
 
