22592b93726995a5c166a48ce70014d417a88a2e
EvaMink <76566995+EvaMink@users.noreply.github.com>
2021-06-03 03:21:45 +0000

    Update compiler.ps1

--- build/compiler/compiler.ps1
+++ build/compiler/compiler.ps1
@@ -2,14 +2,15 @@ echo "Setting up compiler"
 
 
 
-// Windows
+# Windows
 if ((${env:COMPILER} -Match "^win32_") -Eq $True) {
   $x = (${env:COMPILER} -Split "win32_")[1]
 
   run_script "build/compiler/$x.ps1"
 }
 
-// Linux, Mac, Android, Other
+
+# Linux, Mac, Android, Other
 else {
   run_script "build/compiler/${env:COMPILER}.ps1"
 }
