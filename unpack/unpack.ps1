a348faa4626bf5aed20144c4bc7090a8197d5183
EvaMink <76566995+EvaMink@users.noreply.github.com>
2021-07-29 19:12:16 +0000

    Update unpack.ps1

--- unpack/unpack.ps1
+++ unpack/unpack.ps1
@@ -1,19 +1,27 @@
 if ($env:GIT_CLEAN -Eq "yes") {
   cd $env:APPVEYOR_BUILD_FOLDER
 
+  echo "1"
   move $env:GIT_ARCHIVE ..\
+  echo "2"
   move .git* ..\
+  echo "3"
 
 
 
   cd ..
+  echo "4"
   Remove-Item -Recurse -Force $env:APPVEYOR_BUILD_FOLDER
+  echo "5"
   New-Item $env:APPVEYOR_BUILD_FOLDER -itemtype directory
+  echo "6"
 
 
 
   move $env:ARCHIVE $env:APPVEYOR_BUILD_FOLDER
+  echo "7"
   move .git* $env:APPVEYOR_BUILD_FOLDER
+  echo "8"
 
   cd $env:APPVEYOR_BUILD_FOLDER
 }
